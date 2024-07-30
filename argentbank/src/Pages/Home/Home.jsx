48b2a1b112a37bfeaeb9164735b104c8bdf10004
ZakariaDevweb <zakaria.messali25@gmail.com>
2024-07-30 17:06:40 +0200

    home delete cp

--- argentbank/src/Pages/Home/Home.jsx
+++ argentbank/src/Pages/Home/Home.jsx
@@ -3,43 +3,15 @@ import '../../assets/styles/styles.css';
 
 const Home = () => {
   return (
-    <div className="home-page">
-      {/* Header */}
-      <header className="header">
-        <div className="logo">
-          <h1>ARGENTBANK</h1>
-        </div>
-        <div className="sign-in">
-          <a href="/login">Sign In</a>
-        </div>
-      </header>
+    
+      
+        
       <main className="main-content">
-       
-        {/* features */}
-        <div className="features">
-          <div className="feature">
-            <div className="icon chat-icon"></div>
-            <h3>You are our #1 priority</h3>
-            <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
-          </div>
-          <div className="feature">
-            <div className="icon money-icon"></div>
-            <h3>More savings means higher rates</h3>
-            <p>The more you save with us, the higher your interest rate will be!</p>
-          </div>
-          <div className="feature">
-            <div className="icon security-icon"></div>
-            <h3>Security you can trust</h3>
-            <p>We use top of the line encryption to make sure your data and money is always safe.</p>
-          </div>
-        </div>
-      </main>
-      {/* footer  */}
-      <footer className="footer">
-        <p>Copyright 2020 Argent Bank</p>
-      </footer>
-    </div>
-  );
-};
+        
+  </main>
 
-export default Home;
+      
+  );
+};   
+  
+export default Home
