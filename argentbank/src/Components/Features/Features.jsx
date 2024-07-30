37ee2746a85d55d8ede3db824148209bb11ab5d2
ZakariaDevweb <zakaria.messali25@gmail.com>
2024-07-30 16:13:54 +0200

    footer cp

---
+++ argentbank/src/Components/Features/Features.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../assets/styles/styles.css';
+
+const Features = () => {
+    return (
+<div className="features">
+          <div className="feature">
+            <div className="icon chat-icon"></div>
+            <h3>You are our #1 priority</h3>
+            <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
+          </div>
+          <div className="feature">
+            <div className="icon money-icon"></div>
+            <h3>More savings means higher rates</h3>
+            <p>The more you save with us, the higher your interest rate will be!</p>
+          </div>
+          <div className="feature">
+            <div className="icon security-icon"></div>
+            <h3>Security you can trust</h3>
+            <p>We use top of the line encryption to make sure your data and money is always safe.</p>
+          </div>
+        </div>
+    );
+};
+
+
+export default Features;
---
+++ argentbank/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../assets/styles/styles.css';
+
+const Footer = () => {
+    return (
+<div className="Footer">
+<footer className="Footer">
+        <p>Copyright 2020 Argent Bank</p>
+      </footer>
+    </div>
+  );
+};
+
+export default  Footer ;
