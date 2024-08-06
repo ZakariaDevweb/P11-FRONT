4088d4bc60ad97ea2c1fdcaad8726b88f36e53b4
ZakariaDevweb <zakaria.messali25@gmail.com>
2024-08-06 16:58:02 +0200

    footer

--- argentbank/src/Components/Footer/Footer.jsx
+++ argentbank/src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import '../../assets/styles/styles.css';
 
 const Footer = () => {
     return (
-<div className="Footer">
-<footer className="Footer">
+<div className="footer">
+<footer className="footer-text">
         <p>Copyright 2020 Argent Bank</p>
       </footer>
     </div>
